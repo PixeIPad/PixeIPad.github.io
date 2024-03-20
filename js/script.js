d56773364a98a28930a79cbeee2f40028336a295
Naikyy123 <160781714+Vampleysta@users.noreply.github.com>
2024-03-20 20:55:26 +0100

    PixelPad

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 window.addEventListener("load", function(){
-    $(document).ready(function(){
-        $('.carousel').carousel({
-                padding: 200
-        });
+  $(document).ready(function(){
+      $('.carousel').carousel({
+              padding: 200
       });
+    });
+
 });
 
 function changeBg(bg, title){
-    const banner = document.querySelector('.banner');
-    const contents = document.querySelectorAll('.content');
-    banner.style.background = `url("../img/Carousel/${bg}")`;
-    banner.style.backgroundSize = 'cover';
-    banner.style.backgroundPosition = 'center';
+  const banner = document.querySelector('.banner');
+  const contents = document.querySelectorAll('.content');
+  banner.style.background = `url("../img/Carousel/${bg}")`;
+  banner.style.backgroundSize = 'cover';
+  banner.style.backgroundPosition = 'center';
 
-    contents.forEach(content => {
-        content.classList.remove('active');
-        if(content.classList.contains(title)){
-            content.classList.add('active');
-        }
-    });
+  contents.forEach(content => {
+      content.classList.remove('active');
+      if(content.classList.contains(title)){
+          content.classList.add('active');
+      }
+  });
   document.getElementById("search-in").onkeyup = function() {
-      search_animal();
+    search_animal();
   };
-  function search_animal() {
-     
-      var query = document.getElementById("search-in").value;
+  function search_animal() { 
+    var query = document.getElementById("search-in").value;
   }
 }
